1b7df0478b8aa00edac7f3c15dd8d7af358d783b
Kristoffer Brabrand <kristoffer@brabrand.no>
2016-08-12 01:24:21 +0200

    Restructured code

--- browser.js
+++ browser.js
@@ -1,9 +1,7 @@
-var compress = require('./src/compress');
 var assemble = require('./src/assemble');
 
 module.exports = function SuperPreviewClient(opts) {
     return {
-        compress: compress(opts),
         assemble: assemble(opts)
     }
 }
---
+++ src/compress.js
@@ -0,0 +1,62 @@
+var path = require('path');
+var merge = require('lodash.merge');
+
+var imagick;
+var imageHeaders = require('../assets/image-header');
+
+try {
+    //Check if gm (Graphics Magick) is installed
+    require.resolve('gm');
+
+    imagick = require('gm').subClass({
+        imageMagick: true
+    });
+} catch(e) {}
+
+module.exports = function(opts) {
+    opts = opts || {};
+
+    var qTablePath = opts.qTablePath || path.join(__dirname, '..', 'q-table.xml');
+
+    return function compress(inputBuffer, cb) {
+        if (!imagick) {
+            console.warn('The npm module gm is not installed');
+            return;
+        }
+
+        var image = imagick(inputBuffer, 'image.jpg');
+
+        var size = image.size(function (err, size) {
+            var ratio = size.width/size.height;
+
+            if (ratio > 1) {
+                width = 42;
+                height = Math.floor(42 / ratio);
+            } else {
+                width = Math.floor(42 * ratio);
+                height = 42;
+            }
+
+            imagick(inputBuffer, 'image.jpg')
+                .resize(42, 42)
+                .extent(42, 42)
+                .noProfile()
+                .quality(20)
+                .define('jpeg:q-table=' + qTablePath)
+                .define('jpeg:optimize-coding=false')
+                .toBuffer('JPG', function(err, outputBuffer) {
+                    var disposablePortionEnd = outputBuffer.indexOf(new Buffer([0xFF,0xDA]));
+
+                    if (err) {
+                        return cb(err);
+                    }
+
+                    // Return the image data part of the binary data buffer
+                    cb(null,
+                        width + ':' +
+                        height + ':' +
+                        outputBuffer.slice(disposablePortionEnd + 2));
+                });
+        });
+    }
+};
